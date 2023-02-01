89a73725b48a57fcae02929bbecb8d14ca63b225
sheilan102 <sheilan102@yahoo.com>
2023-02-01 13:03:47 +0200

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { register } from './serviceWorkerRegistration';
 
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt event has fired')
+  e.prompt()
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+register();
